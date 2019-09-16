246f60df7ccd1c1f19b343a165fccdac1551a529
Mohamed <mr.tabal@yahoo.co.uk>
2019-09-16 10:20:10 +0100

    set up

--- knexfile.js
+++ knexfile.js
@@ -13,16 +13,16 @@ const baseConfig = {
 const customConfig = {
   development: {
     connection: {
-      database: 'nc_news'
-      // username,
-      // password
+      database: 'nc_news',
+      username: "mohamed",
+      password: 'password'
     }
   },
   test: {
     connection: {
-      database: 'nc_news_test'
-      // username,
-      // password
+      database: 'nc_news_test',
+      username: "mohamed",
+      password: 'password'
     }
   }
 };
