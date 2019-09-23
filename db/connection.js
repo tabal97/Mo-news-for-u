59b3452e8969d98248d903fc917fa19795d239ab
Mohamed <mr.tabal@yahoo.co.uk>
2019-09-23 11:30:50 +0100

    moved knex to user dependency

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,5 @@ const dbConfig =
     ENV === 'production'
         ? { client: 'pg', connection: process.env.DATABASE_URL }
         : require('../knexfile');
-const connection = knex(dbConfig);
 
-module.exports = connection;
+module.exports = knex(dbConfig);
