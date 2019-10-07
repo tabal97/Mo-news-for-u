a3e6a5946a40873f09af0c3c7fe6f257f2a99eb9
Mohamed <mr.tabal@yahoo.co.uk>
2019-10-07 16:16:03 +0100

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require("cors")
 const app = express();
 const apiRouter = require("./routers/api");
 const { badRequestErrorHandler, internalServerErrorHandler, notFoundErrorHandler } = require("./errorHandlers")
 
 app.use(express.json());
-
+app.use(cors())
 app.use("/api", apiRouter);
 
 app.all("/*", (req, res, next) => {
